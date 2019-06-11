d3db4a3730025a6b04face4c630a8fdd84454a42
wk-j <somnuk.wk@gmail.com>
2019-06-11 12:57:09 +0700

    Deserialize

---
+++ src/Deserialize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Library;
+using Newtonsoft.Json;
+
+namespace Deserialize {
+    class Program {
+        static void Main(string[] args) {
+            var xml = File.ReadAllText("resource/Generate.xml");
+            var element = XElement.Parse(xml);
+            var reader = element.CreateReader();
+            var serializer = new XmlSerializer(typeof(Envelope));
+            var obj = serializer.Deserialize(reader);
+            var json = JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
--- src/Namespace/Program.cs
+++ src/Namespace/Program.cs
@@ -2,42 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
-
-[XmlRoot(Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-public class Envelope {
-    [XmlElement]
-    public Header Header { set; get; }
-    [XmlElement]
-    public Body Body { set; get; }
-}
-
-[XmlRoot(Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-public class Body {
-    [XmlElement("OrganizationInfo", Namespace = "")]
-    public List<Organization> OrganizationList { set; get; }
-}
-
-[XmlRoot(Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-public class Header {
-    [XmlElement("MessageID")]
-    public string MessageId { set; get; }
-    public string To { set; get; }
-}
-
-public class Organization {
-    [XmlElement("Th-Name")]
-    public string ThName { set; get; }
-
-    [XmlElement("En-Name")]
-    public string EnName { set; get; }
-
-    [XmlElement("Ecms-Url")]
-    public string EcmsUrl { set; get; }
-
-    [XmlElement("OrganizationID")]
-    public string OrganizationId { set; get; }
-}
-
+using Library;
 
 namespace Namespace {
     class Program {
