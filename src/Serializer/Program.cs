e9047e651ea67de6172404cc4e7a91280cbd48b3
wk-j <somnuk.wk@gmail.com>
2019-06-11 11:50:06 +0700

    Initial commit

---
+++ src/Serializer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
