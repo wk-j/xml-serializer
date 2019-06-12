22c4fc18502c679f50e97ed8ed88da0607c5b900
wk-j <somnuk.wk@gmail.com>
2019-06-12 17:33:01 +0700

    Abstract

---
+++ src/Abstract/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Abstract {
+
+    public class NS {
+        public const string Envelope = "http://schemas.xmlsoap.org/soap/envelope/";
+        public const string Addressing = "http://www.w3.org/2005/08/addressing";
+        public const string Empty = "";
+    }
+
+    [XmlRoot(Namespace = NS.Envelope)]
+    public class Header {
+        [XmlElement("MessageID", Namespace = NS.Addressing)]
+        public string MessageId { set; get; }
+
+        [XmlElement("To", Namespace = NS.Addressing)]
+        public string To { set; get; }
+    }
+
+    [XmlRoot(Namespace = NS.Envelope)]
+    public class Envelope {
+        [XmlElement]
+        public Header Header { set; get; }
+        public Body Body { set; get; }
+    }
+
+    [XmlRoot(Namespace = NS.Envelope)]
+    public class Body {
+        [XmlElement(Namespace = NS.Empty)]
+        public Response CorrespondenceLetterInboundResponse { set; get; }
+    }
+
+    public class Response {
+        [XmlElement("ProcessID")]
+        public string ProcessId { set; get; }
+        public DateTime ProcessTime { set; get; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
