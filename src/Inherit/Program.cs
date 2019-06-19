08536b3bc07604c851221a8cf8fca684bc5cd7c2
wk-j <somnuk.wk@gmail.com>
2019-06-19 13:21:47 +0700

    Inherit

---
+++ src/Inherit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Inherit {
+    public class A {
+        [XmlElement("ProcessID")]
+        public string ProcessId { set; get; }
+
+    }
+
+    public class B : A {
+        public string ProcessTime { set; get; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var s = new XmlSerializer(typeof(B));
+            var stream = new MemoryStream();
+            var b = new B {
+                ProcessId = "001",
+                ProcessTime = "001"
+            };
+
+            s.Serialize(stream, b);
+
+            stream.Seek(0, SeekOrigin.Begin);
+            var str = new StreamReader(stream).ReadToEnd();
+
+            Console.WriteLine(str);
+        }
+    }
+}
